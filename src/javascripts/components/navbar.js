0201dedffb39e3b667c8b781aca90845c5ea2465
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-03-06 15:53:31 -0600

    Updated navbar with Bootstrap

---
+++ src/javascripts/components/navbar.js
@@ -0,0 +1,23 @@
+const navbar = () => {
+  document.querySelector('#navbar-container').innerHTML = `<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #ee8c84;">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a class="navbar-brand" href="#">Lindsey Satterfield</a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item">
+        <a class="nav-link" href="#navToBio">Bio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#navToTechnologies">Technologies</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#navToContact">Contact Me</a>
+      </li>
+    </ul>
+  </div>
+</nav>`;
+};
+
+export default navbar;
