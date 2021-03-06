64934b530136e314ebf3ff5cb7d1ddc4db2274dd
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-03-06 16:35:55 -0600

    Updated profile area

---
+++ src/javascripts/components/profile.js
@@ -0,0 +1,14 @@
+import coding from '../../assets/coding.png';
+
+const profile = () => {
+  document.querySelector('#profile').innerHTML = `
+    <h1 class="greeting">Hello World! I'm Lindsey.</h1>
+    <h3 class="greeting-title">Web Developer</h3>
+    <img src="${coding}" id="coding-img" alt="Computer with code">
+    <div id="bio">
+      <p class="bio">As a long time enthusiast of all things technology, I am currently loving what I do -- developing 
+      websites and applications. A Nashville native, I am still here and currently attending Nashville Software School. Formerly a worker in the video production world and graduate of the University of Tennessee, I decided it was time to follow my dreams and enter the technology world to allow myself more creativity. I am in the process of learning Front End Development and C#/.NET. When I am not in front of a computer, I enjoy hiking, photography, traveling, and going to Preds games.</p> 
+    </div>`;
+};
+
+export default profile;
